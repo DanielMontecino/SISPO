214313f12201a0c4cf0fee0c93b0cfa865165223
bldr <ad@ad.com>
2018-12-16 14:06:11 -0300

    heatmap

--- realplot.py
+++ realplot.py
@@ -1,5 +1,7 @@
 import serial
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
 import matplotlib.animation as animation
 import time
 import numpy as np
@@ -21,6 +23,8 @@ ax1.set_ylim([0, 100])
 points = np.linspace(0,100,200)
 cincuentas = np.ones(200)*50
 radio = 20
+X_HIST = []
+Y_HIST = []
 
 def animate(i):
 
@@ -30,8 +34,10 @@ def animate(i):
             x = float(rawString.decode('utf-8').split(';')[0])
             y = float(rawString.decode('utf-8').split(';')[1])
         else:
-            x = 50 + np.random.normal(scale=3)
-            y = 50 + np.random.normal(scale=10)
+            x = 50 + np.random.normal(scale=25)
+            y = 50 + np.random.normal(scale=25)
+        X_HIST.append(x)
+        Y_HIST.append(y)
         if (x-50)**2 + (y-50)**2<= radio**2:
             color = 'b'
         else:
@@ -55,3 +61,19 @@ def animate(i):
 
 ani = animation.FuncAnimation(fig, animate, interval=10)
 plt.show()
+
+#heatmap
+fig = plt.figure(figsize=(5,5))
+resolution = np.linspace(0,100,20)
+H, xedges, yedges  = np.histogram2d(X_HIST, Y_HIST, bins=(resolution,resolution), normed =True)
+H = H.T
+ax = fig.add_subplot(111, title='NonUniformImage: interpolated', aspect='equal', xlim=xedges[[0, -1]], ylim=yedges[[0, -1]])
+im = mpl.image.NonUniformImage(ax, interpolation='bilinear',cmap=cm.Purples)
+xcenters = (xedges[:-1] + xedges[1:]) / 2
+ycenters = (yedges[:-1] + yedges[1:]) / 2
+circle1 = ax.add_artist(plt.Circle((50, 50), radio, color='r', fill=False,alpha=0.5))
+ax.scatter(points, cincuentas, s = 0.5, c='k', alpha=0.5)
+ax.scatter(cincuentas, points, s = 0.5, c='k' , alpha=0.5)
+im.set_data(xcenters, ycenters, H)
+ax.images.append(im)
+plt.show(im)
